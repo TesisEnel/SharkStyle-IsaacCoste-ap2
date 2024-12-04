81b2e75455f37858b667a17f34ce39b46250632c
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-12-04 13:48:36 -0400

    Instalando dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,15 @@ plugins {
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.plugin.compose")
     alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
-    namespace = "com.example.sharkstyle"
+    namespace = "com.example.pruebasharkstyle"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.sharkstyle"
+        applicationId = "com.example.pruebasharkstyle"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
@@ -24,7 +25,6 @@ android {
     room {
         schemaDirectory("$projectDir/schemas")
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -35,11 +35,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
@@ -56,8 +56,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.material)
-    implementation(libs.androidx.benchmark.macro)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -90,4 +90,13 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
+    implementation(libs.androidx.material.icons.extended)
+    implementation("com.google.code.gson:gson:2.10")
+
+
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
@@ -7,4 +6,5 @@ plugins {
     id("androidx.room") version "2.6.1" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply false        //aqui se busca la ultima version https://github.com/google/ksp/releases
     id("com.google.dagger.hilt.android") version "2.51" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
